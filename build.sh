fc3df8aa26a922d91a31a84d92d994cb7643ecff
Aaron <williamsaquinopoma@gmail.com>
2024-03-15 23:17:53 -0500

    FinalAlFIn

--- build.sh
+++ build.sh
@@ -9,6 +9,13 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 
+correo_electronico="clv@gmail.com"
+contrasena="contrasegura"
+
+# Crear un superusuario
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('${correo_electronico}', '${contrasena}')" | python manage.py shell
+
+echo "Superusuario creado con éxito."
 
 #Puedes agregar más comandos según sea necesario
 #Otros comandos que puedas necesitar ejecutar después de la creación del superusuario
