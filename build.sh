0a6b695a32d34cd00b58fce259c5db055cebab63
Aaron <williamsaquinopoma@gmail.com>
2024-03-15 22:00:10 -0500

    despliegue

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+exit on error
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+
+
+nombre_de_usuario="clvsuper"
+correo_electronico="xdxd@ejemplo.com"
+contrasena="contrasegura"
+
+Verificar si el superusuario ya existe
+if python manage.py shell -c "from django.contrib.auth.models import User; print(User.objects.filter(username='${nombre_de_usuario}', is_superuser=True).exists())" | grep "True" &> /dev/null; then
+    echo "El superusuario ya existe. No se creará uno nuevo."
+else
+    # Crear un superusuario
+    echo "from django.contrib.auth.models import User; User.objects.create_superuser('${nombre_de_usuario}', '${correo_electronico}', '${contrasena}')" | python manage.py shell
+    echo "Superusuario creado con éxito."
+fi
+
+Puedes agregar más comandos según sea necesario
+Otros comandos que puedas necesitar ejecutar después de la creación del superusuario
+...
+Finalizar con un mensaje
+echo "¡Script de construcción completado con éxito!"
--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os, dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^ga48hys#j+b^lcb=%i_@$%6o#i=v@y!+faq&ao$eto^8c53pf'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='defaultsecretkey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
@@ -52,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -79,14 +85,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'clvapidb',
-        'USER' : 'postgres',
-        'PASSWORD' : 'root',
-        'HOST' : 'localhost',
-        'PORT' : '5432'
-    }
+    'default': dj_database_url.config()
 }
 
 
@@ -125,6 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
