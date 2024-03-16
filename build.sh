dba43762cbe4bc4fd769590960e7e096b0d13423
Aaron <williamsaquinopoma@gmail.com>
2024-03-15 23:10:27 -0500

    XDDDDD

--- build.sh
+++ build.sh
@@ -12,6 +12,6 @@ python manage.py migrate
 
 #Puedes agregar más comandos según sea necesario
 #Otros comandos que puedas necesitar ejecutar después de la creación del superusuario
-...
+
 #Finalizar con un mensaje
 echo "¡Script de construcción completado con éxito!"
