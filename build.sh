feacb3c4ff9038abd2f7ef4577423a39a09f1cd2
Aaron <williamsaquinopoma@gmail.com>
2024-03-15 22:21:13 -0500

    erraresdenovatos

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-exit on error
+#exit on error
 set -o errexit
 
 pip install -r requirements.txt
@@ -13,7 +13,7 @@ nombre_de_usuario="clvsuper"
 correo_electronico="xdxd@ejemplo.com"
 contrasena="contrasegura"
 
-Verificar si el superusuario ya existe
+#Verificar si el superusuario ya existe
 if python manage.py shell -c "from django.contrib.auth.models import User; print(User.objects.filter(username='${nombre_de_usuario}', is_superuser=True).exists())" | grep "True" &> /dev/null; then
     echo "El superusuario ya existe. No se creará uno nuevo."
 else
@@ -22,8 +22,8 @@ else
     echo "Superusuario creado con éxito."
 fi
 
-Puedes agregar más comandos según sea necesario
-Otros comandos que puedas necesitar ejecutar después de la creación del superusuario
+#Puedes agregar más comandos según sea necesario
+#Otros comandos que puedas necesitar ejecutar después de la creación del superusuario
 ...
-Finalizar con un mensaje
+#Finalizar con un mensaje
 echo "¡Script de construcción completado con éxito!"
