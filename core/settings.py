443784565ab839da886c56615cef23529049c8f4
Aaron <williamsaquinopoma@gmail.com>
2024-03-17 09:32:42 -0500

    asdsadsadasd

--- core/settings.py
+++ core/settings.py
@@ -63,8 +63,8 @@ MIDDLEWARE = [
     
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
-#CORS_ALLOWED_ORIGINS = ['http://localhost:8000', 'http://localhost:4200']
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ALLOWED_ORIGINS = ['http://localhost:8000', 'http://localhost:4200']
 
 
 ROOT_URLCONF = 'core.urls'
